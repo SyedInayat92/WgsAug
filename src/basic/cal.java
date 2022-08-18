6b54d2aa863baf84c9c2e9b957317da967552228
Sayyed.Inayatalam@Worldline.com <sayyed.inayatalam@worldline.com>
2022-08-18 03:27:57 -0700

    added math and cal

---
+++ src/basic/cal.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class cal {
+	 
+	public static void main(String [] args)
+	{
+		System.out.println("Addition");
+		
+		math.add(22,67);
+		
+		math.add(65,89,43);
+		
+		System.out.println("Mutliplication");
+		
+		math.multiply(23,8);
+	}
+}
---
+++ src/basic/math.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class math {
+	
+	public static void add(int a, int b)
+	{
+		System.out.println("Sum  :" + a+b);
+	}
+	
+	public  static void add(int a, int b,int c)
+	{
+		System.out.println("Sum  :" + a+b+c);
+	}
+	
+	public  static void multiply(int a, int b)
+	{
+		System.out.println("Product :" + a*b);
+	}
+	
+	
+}
